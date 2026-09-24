a778fcb031f82c5439ccba70e6d3018c31da18c3
alexandermbh27-collab <alexandermbh27@gmail.com>
2026-09-23 21:21:04 -0600

    Publicar aplicación móvil de Territorios Moderna

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const VERSION='moderna-pwa-v1';
+const ARCHIVOS=['./','./index.html','./manifest.webmanifest','./icon.svg','./territorio-moderna.kml'];
+self.addEventListener('install',evento=>evento.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(ARCHIVOS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',evento=>evento.waitUntil(caches.keys().then(llaves=>Promise.all(llaves.filter(llave=>llave!==VERSION).map(llave=>caches.delete(llave)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',evento=>{
+  if(evento.request.method!=='GET')return;
+  evento.respondWith(fetch(evento.request).then(respuesta=>{const copia=respuesta.clone();caches.open(VERSION).then(cache=>cache.put(evento.request,copia));return respuesta}).catch(()=>caches.match(evento.request)))
+});
